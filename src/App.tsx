25a23a232df17ea7a66d60d1d1867a17ea42f773
maryelLb <MaryelLB1999@gmail.com>
2025-05-09 19:35:47 -0600

    [Add] Create the hook of the order

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 
 export default function App() {
+  const { AddItem } = useOrder()
 
   return (
     <>
@@ -17,6 +19,7 @@ export default function App() {
             <MenuItem
               key={item.id} 
               item = {item}
+              addItem={AddItem}
             />
           ))}
           </div>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,12 +1,14 @@
 import type { MenuItems } from "../types"
 
 type MenuItemProps = {
-    item: MenuItems
+    item: MenuItems,
+    addItem: () => void
 }
-export default function MenuItem({item}: MenuItemProps) {
+export default function MenuItem({item, addItem}: MenuItemProps) {
     return (
         <button
         className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+        onClick={() => addItem()}
         >
         <p>{item.name}</p>
         <p className="font-black">${item.price}</p>
