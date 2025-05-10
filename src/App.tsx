edece306088ee5c8abcad4b620db0d50ce433d76
maryelLb <MaryelLB1999@gmail.com>
2025-05-09 18:54:21 -0600

    [Add] The elements of the project Tip Calculator

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { MenuItems } from "./types"
+export default function App() {
+
+  return (
+    <>
+    <header className="bg-teal-400 py-5">
+      <h1 className="text-center text-3xl font-black uppercase">Calculadora de propinas y consumo</h1>
+    </header>
+    </>
+  )
+}
+
+
